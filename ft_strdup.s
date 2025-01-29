8f60eed0e7fb632be7f49810726044a5029e4103
xmatute- <xmatute-@student.42urduliz.com>
2025-01-29 17:17:31 +0100

    strdup done

--- ft_strdup.s
+++ ft_strdup.s
@@ -1,9 +1,22 @@
 section .text
-    global ft_strdup
-    extern malloc
+	global	ft_strdup
+	extern	malloc			; # include malloc
+	extern	ft_strlen       ; # include ft_strlen
+	extern	ft_strcpy       ; # include ft_strcpy
 
-ft_strdup:
-    ; Solicitar 1 byte de memoria usando malloc
-    mov rdi, 1          
-    call malloc wrt ..plt 
-    ret
+ft_strdup:					; char *ft_strdup(const char *src) rdi = src
+    ; mov rsi, rdi          ; esto no vale hay que hacer push pop
+	push	rdi				; pile.push(src)
+	call	ft_strlen		; rax = ft_strlen(src)
+	inc		rax				; rax++
+	mov		rdi, rax		; rdi = rax 
+	call	malloc	wrt ..plt; rax = malloc(rax) = malloc(strlen(src) + 1)
+	cmp		rax, 0			; if rax == 0 
+	je		.null			; return null
+	pop		rsi				; rsi = pile.pop() = src
+	mov		rdi, rax		; rdi = rax
+	call	ft_strcpy		; rax = ft_strcpy(rdi, rsi)
+	ret						; returns rax
+
+.null:
+	ret						; returns rax (0)
