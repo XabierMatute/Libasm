e538083ae6ba366f4f4538a8d5b0b640e3b9cbba
xmatute- <xmatute-@student.42urduliz.com>
2025-01-29 17:28:01 +0100

    syscall errno gestionado

--- ft_read.s
+++ ft_read.s
@@ -1,7 +1,19 @@
 section .text
 global ft_read
+extern __errno_location	; includes ___error but for linux
 
 ft_read:
     mov rax, 0      ; rax (acumulador/output/tipo de llamada) = 0
     syscall         ; Invocar syscall(0 read)
+
+    cmp rax, 0      ; if rax(acumulador/output/read return value) < 0
+    jge .piola      ; go to piola
+
+    neg rax         ; rax = -rax
+    mov rdi, rax    ; rdi = rax
+    call __errno_location ; Establecer errno
+    mov [rax], rdi  ; Establecer errno
+    ret             ; return rax(acumulador/output/read return value)
+
+.piola:
     ret             ; return rax(acumulador/output/read return value)
--- ft_write.s
+++ ft_write.s
@@ -1,9 +1,20 @@
 section .text
 global ft_write
+extern __errno_location	; includes ___error but for linux
 
 ft_write:
     mov rax, 1      ; rax(acumulador/output/tipo de llamada) = 1
 
-    syscall         ; invoke syscall(1 write)
+    syscall         ; Invocar syscall(1 write)
 
+    cmp rax, 0      ; if rax(acumulador/output/write return value) < 0
+    jge .piola      ; go to piola
+
+    neg rax         ; rax = -rax
+    mov rdi, rax    ; rdi = rax
+    call __errno_location ; Establecer errno
+    mov [rax], rdi  ; Establecer errno
+    ret             ; return rax(acumulador/output/write return value)
+
+.piola:
     ret             ; return rax(acumulador/output/write return value)
